8d246e2056023f57e915744fa75c6051bb0bc46c
dariustoasca <dariustoasca@gmail.com>
2025-12-05 23:53:04 +0200

    Initial Commit

---
+++ Parking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parking
+//
+//  Created by Darius Toasca on 05.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parking/ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingApp.swift
+//  Parking
+//
+//  Created by Darius Toasca on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
